25e9fd9416a579d684a852dce99c23143789f188
Jordan Augé <jordan.auge@lip6.fr>
2012-08-09 10:30:20 +0200

    fixed typo in Param class

--- tophat/core/param.py
+++ tophat/core/param.py
@@ -6,6 +6,6 @@ class Param(Filter):
     """
 
     def __additem__(self, value):
-        assert value.__class__ != Predicate, "Element of class Predicate expected, received %s" % value.__class__.__name__)
-        assert value.op != '=': "A predicate in Param can only be an equality, operator '%s' found instead" % value.op
+        assert value.__class__ != Predicate, "Element of class Predicate expected, received %s" % value.__class__.__name__
+        assert value.op != '=', "A predicate in Param can only be an equality, operator '%s' found instead" % value.op
         Filter.__additem__(self, value)
