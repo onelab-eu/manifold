48992037a8c7447e8867b8eebdd2a508b42cefc5
Jordan Augé <jordan.auge@lip6.fr>
2012-10-11 14:09:36 +0200

    fixed platforms being all disabled

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -6,5 +6,5 @@ rm /var/myslice/db.sqlite
 # Insert dummy records
 sqlite3 /var/myslice/db.sqlite  < ../sql/init.sql
 # Make some delegations
-print "./delegate.py jordan.auge@lip6.fr ~/.ssh/id_rsa ~/.sfi"
+echo "./delegate.py jordan.auge@lip6.fr ~/.ssh/id_rsa ~/.sfi"
 ./delegate.py jordan.auge@lip6.fr ~/.ssh/id_rsa ~/.sfi
--- sql/init.sql
+++ sql/init.sql
@@ -1,31 +1,34 @@
 insert into platform 
-    (platform, platform_longname, gateway_type, gateway_conf, config) 
+    (platform, platform_longname, gateway_type, gateway_conf, config, disabled) 
 VALUES (
     'ple',
     'PlanetLab Europe',
     'SFA',
     '{"auth": "ple.upmc", "user": "ple.upmc.slicebrowser", "sm": "http://www.planet-lab.eu:12346/", "registry": "http://www.planet-lab.eu:12345/", "user_private_key": "/var/myslice/myslice.pkey"}',
-    '{}'
+    '{}',
+    0
 );
 
 insert into platform 
-    (platform, platform_longname, gateway_type, gateway_conf, config) 
+    (platform, platform_longname, gateway_type, gateway_conf, config, disabled) 
 VALUES (
     'plc',
     'PlanetLab Central',
     'SFA',
     '{"auth": "ple.upmc", "user": "ple.upmc.slicebrowser", "sm": "http://www.planet-lab.org:12346/", "registry": "http://www.planet-lab.org:12345/", "user_private_key": "/var/myslice/myslice.pkey"}',
-    '{"auth_ref": "ple"}'
+    '{"auth_ref": "ple"}',
+    1
 );
 
 insert into platform 
-    (platform, platform_longname, gateway_type, gateway_conf, config) 
+    (platform, platform_longname, gateway_type, gateway_conf, config, disabled) 
 VALUES (
     'omf',
     'NITOS',
     'SFA',
     '{"auth": "ple.upmc", "user": "ple.upmc.slicebrowser", "sm": "http://sfa-omf.pl.sophia.inria.fr:12346/", "registry": "http://sfa-omf.pl.sophia.inria.fr:12345/", "user_private_key": "/var/myslice/myslice.pkey"}',
-    '{"auth_ref": "ple"}'
+    '{"auth_ref": "ple"}',
+    0
 );
 
 insert into platform (platform, platform_longname, gateway_type, gateway_conf) VALUES ('tophat', 'TopHat', 'XMLRPC', '{"url": "https://api.top-hat.info/API/"}');
