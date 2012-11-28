11976ed0f1b8904552bc8d0ab421994f019b52dd
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-11-28 17:12:36 +0100

    changed order of arguments accept multiple delegated creds help displays example with ~/.fsfi/*_for_.cred url can be specified on the command line

--- tophat/bin/uploadcredential.py
+++ tophat/bin/uploadcredential.py
@@ -1,39 +1,52 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
-MYSLICE_API = "http://demo.myslice.info:7080"
+MYSLICE_DEFAULT_API = "http://demo.myslice.info:7080"
+
+from optparse import OptionParser
 
 import sys
 import xmlrpclib
 import getpass
 
-def usage():
-    print "Usage: %s CREDENTIAL PLATFORM API_USERNAME" % sys.argv[0]
-    print ""
-    print "Uploads a credential to MySlice"
-    print "    CREDENTIAL"
-    print "    PLATFORM"
-    print "    API_USERNAME"
+#def usage():
+#    print "Usage: %s CREDENTIAL PLATFORM API_USERNAME" % sys.argv[0]
+#    print ""
+#    print "Uploads a credential to MySlice"
+#    print "    CREDENTIAL"
+#    print "    PLATFORM"
+#    print "    API_USERNAME"
 
 def main():
-    argc = len(sys.argv)
-    if argc != 4:
-        usage()
+    usage="""%prog [--url apiurl] platform api_username credential_1 .. credential_n
+  Uploads a set of credentials to MySlice
+Example:
+  %prog ple my_login ~/.sfi/*_for*.cred"""
+    parser=OptionParser (usage=usage)
+    parser.add_option ("-u","--url",dest='url',default=MYSLICE_DEFAULT_API,
+                       help="Specify API url (default is %s)"%MYSLICE_DEFAULT_API)
+    (options,args)=parser.parse_args()
+
+    if len(args)<=2: 
+        parser.print_help()
         sys.exit(1)
 
-    credential = open(sys.argv[1]).read()
-    platform = sys.argv[2]
-    api_username = sys.argv[3]
-    api_password = getpass.getpass("Enter your API password: ")
+    platform=args.pop(0)
+    api_username=args.pop(0)
+    delegated_credential_files=args
 
-    # Uploading credentials to MySlice
+    api_password = getpass.getpass("Enter your API password for %s: "%api_username)
     auth = {'AuthMethod': 'password', 'Username': api_username, 'AuthString': api_password}
 
-    try:
-        MySlice = xmlrpclib.Server(MYSLICE_API, allow_none = 1)
-        MySlice.AddCredential(auth, credential, platform)
-    except Exception, e:
-        print "E: Error uploading credential: %s" % e
+    for delegated_credential_file in delegated_credential_files:
+        try:
+            delegated_credential = open(delegated_credential_file).read()
+            # Uploading credentials to MySlice
+            MySlice = xmlrpclib.Server(options.url, allow_none = 1)
+            retcod=MySlice.AddCredential(auth, delegated_credential, platform)
+            print delegated_credential_file,'upload retcod',retcod
+        except Exception, e:
+            print "E: Error uploading credential %s: %s" % (delegated_credential_file,e)
 
 if __name__ == '__main__':
     main()
