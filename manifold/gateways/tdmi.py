f194c859e120db1ee46dc7ec6ecdb278f6bdb4f3
Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
2013-03-06 07:12:30 +0100

    merge etc

--- manifold/gateways/tdmi.py
+++ manifold/gateways/tdmi.py
@@ -3,26 +3,26 @@ from __future__ import absolute_import
 
 from manifold.gateways              import Gateway
 from manifold.gateways.postgresql   import PostgreSQLGateway
-from tdmi.methods                   import Traceroute
-
-METHOD_MAP = {
-    'traceroute': Traceroute
-}
-
 # Asynchronous support
 # http://initd.org/psycopg/docs/advanced.html
 
 class TDMIGateway(PostgreSQLGateway):
 
+   
     # The difference is that we forge somehow a part of the SQL request... to be
     # analyzed
 
     def __init__(self):
+        from tdmi.methods                   import Traceroute
+
+        self.METHOD_MAP = {
+            'traceroute': Traceroute
+        }
         self.db = PostgreSQL()
 
     def execute(self, query, user=None):
-        if query.fact_table in METHOD_MAP.keys():
-            return METHOD_MAP[query.fact_table](query, db=self.db)
+        if query.fact_table in self.METHOD_MAP.keys():
+            return self.METHOD_MAP[query.fact_table](query, db=self.db)
 
         # Generic query handling
         # What about subqueries ?
