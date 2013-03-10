abcf292a6e354e72e320f493fe2cc8b069d5c691
Jordan Augé <jordan.auge@lip6.fr>
2013-03-10 16:57:33 +0100

    new manifold component + psql gateway

---
+++ manifold/test/test-gateway-postgresql.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from manifold.gateways.postgresql import PostgreSQL
+
+
--- tophat/gateways/__init__.py
+++
@@ -1,3 +0,0 @@
-from tophat.gateways.xmlrpc import XMLRPC
-from tophat.gateways.sfa import SFA
-from tophat.gateways.maxmind import MaxMind
