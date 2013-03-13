5ebfef1de71744bd076022f034734c90f49762c0
Jordan Augé <jordan.auge@lip6.fr>
2013-03-13 12:02:50 +0100

    fixed typos

--- manifold/core/forwarder.py
+++ manifold/core/forwarder.py
@@ -1,10 +1,10 @@
 from manifold.core.interface    import Interface
-from manifold.core.query_plane  import QueryPlane
+from manifold.core.query_plan  import QueryPlan
 
 class Forwarder(Interface):
 
     def forward(self, query, deferred=False, user=None):
-        qp = QueryPlane()
+        qp = QueryPlan()
         qp.build_simple(query, self.metadata)
         return qp.execute()
 
