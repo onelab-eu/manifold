bc513af6b0d7e65ae72bbf74757b720b56e263b6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-04-18 17:03:50 +0200

    default is to use the new API, use -o/--old to resort to legacy API

--- tophat/bin/uploadcredential.py
+++ tophat/bin/uploadcredential.py
@@ -3,6 +3,13 @@
 
 DEFAULT_MYSLICE_API = "http://demo.myslice.info:7080"
 
+# xxx
+# this now has a user option (--old) so we can use either AddCredential (legacy)
+# or Update() (new/current) to update credentials
+# this option however should only be temporary as we could either
+# (*) sense the flavour of the API we talk to (is there a GetVersion ?)
+# (*) or drop support for old APIs altogether on the longer run
+
 from optparse import OptionParser
 
 import sys
@@ -10,13 +17,17 @@ import xmlrpclib
 import getpass
 
 def main():
-    usage="""%prog [--url apiurl] platform api_username credential_1 .. credential_n
+    usage="""%prog [options] platform api_username credential_1 .. credential_n
   Uploads a set of credentials to MySlice
 Example:
   %prog ple my_login ~/.sfi/*_for*.cred"""
     parser=OptionParser (usage=usage)
-    parser.add_option ("-u","--url",dest='url',default=DEFAULT_MYSLICE_API,
+    parser.add_option ("-u","--url",dest='url',action='store',default=DEFAULT_MYSLICE_API,
                        help="Specify API url (default is %s)"%DEFAULT_MYSLICE_API)
+    parser.add_option ("-o","--old",action='store_true',dest='old_api',default=False,
+                       help="If your backend runs old API (using AddCredential)")
+    parser.add_option ("-n","--new",action='store_false',dest='old_api',
+                       help="If your backend runs new API (no AddCredential anymore)")
     (options,args)=parser.parse_args()
 
     if len(args)<=2: 
@@ -33,12 +44,30 @@ Example:
     for delegated_credential_file in delegated_credential_files:
         try:
             delegated_credential = open(delegated_credential_file).read()
-            # Uploading credentials to MySlice
-            MySlice = xmlrpclib.Server(options.url, allow_none = 1)
-            retcod=MySlice.AddCredential(auth, delegated_credential, platform)
-            print delegated_credential_file,'upload retcod',retcod
+            manifold = xmlrpclib.Server(options.url, allow_none = 1)
+            if options.old_api:
+                # Uploading credentials to MySlice
+                retcod=manifold.AddCredential(auth, delegated_credential, platform)
+                if retcod==1:
+                    print delegated_credential_file,'upload OK'
+                else:
+                    print delegated_credential_file,'upload retcod',retcod
+            else:
+                query= { 'action':       'update',
+                         'fact_table':   'local:account',
+                         'filters':      [ ['platform', '=', platform] ] ,
+                         'params':       {'credential': delegated_credential, },
+                         }
+                retcod=manifold.Update (auth, query)
+                if retcod['code']==0:
+                    print delegated_credential_file,'upload OK'
+                else:
+                    print delegated_credential_file, "upload failed,",retcod['output'], \
+                        "with code",retcod['code']
         except Exception, e:
             print "E: Error uploading credential %s: %s" % (delegated_credential_file,e)
+            import traceback
+            traceback.print_exc()
 
 if __name__ == '__main__':
     main()
