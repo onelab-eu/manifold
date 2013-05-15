c9f5d80389a53500b26786cf2ff2f2e1465e010d
Loic Baron <loic.baron@lip6.fr>
2013-05-15 15:48:39 +0200

    added script sample get-network

---
+++ sample/get-network.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+import pprint
+import xmlrpclib
+from config import auth
+
+err_str = {
+    0: "OK",
+    1: "WARNING",
+    2: "ERROR"
+}
+
+srv = xmlrpclib.Server("http://localhost:7080/", allow_none = True)
+
+def print_err(err):
+    print '-'*80
+    print 'Exception', err['code'], 'raised by', err['origin'], ':', err['description']
+    for line in err['traceback'].split("\n"):
+        print "\t", line
+    print ''
+#q = {'action': 'get', 'fields': ['network_hrn'], 'object': 'network', 'params': [], 'filters': []}
+q = {'action': 'get', 'fields': ['network_hrn'], 'object': 'network'}
+
+ret = srv.forward(auth, q)
+print "====> NETWORKS"
+pprint.pprint(ret)
+
+print "RETURN VALUE:", err_str[ret['code']]
+
+if 'description' in ret:
+    if isinstance(ret['description'], list):
+        # We have a list of errors
+        for err in ret['description']:
+            print_err(err)
+    else:
+        print ret['description']
+
+if 'value' in ret:
+    print "***** Result:"
+    pprint.pprint(ret['value'])
