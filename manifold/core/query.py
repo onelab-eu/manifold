4b418ca8b94f5073c096da2dc018ebdae66aac3a
Jordan Augé <jordan.auge@lip6.fr>
2013-07-22 14:29:19 +0200

    removed useless print messages

--- manifold/core/query.py
+++ manifold/core/query.py
@@ -389,23 +389,19 @@ class Query(object):
             
     def select(self, *fields):
 
-        print "fields in get_select", fields
         # Accept passing iterables
         if len(fields) == 1:
             tmp, = fields
-            print "TMP get_select=", tmp
             if not tmp:
                 fields = None
             elif isinstance(tmp, (list, tuple, set, frozenset)):
                 fields = tuple(tmp)
-                print "modified fields to", fields
 
         if not fields:
             # Delete all fields
             self.fields = set()
             return self
 
-        print "FINALLY In get_select we iterate on", fields
         for field in fields:
             self.fields.add(field)
         return self
