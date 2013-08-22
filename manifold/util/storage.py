2f2e59b822af7386a65ebf343f6ca9ea060c789d
Jordan Augé <jordan.auge@lip6.fr>
2013-08-22 16:05:21 +0200

    improved query plan + support for conf gateway

--- manifold/util/storage.py
+++ manifold/util/storage.py
@@ -21,7 +21,7 @@ class DBStorage(Storage):
     @classmethod
     def execute(self, query, user=None, format='dict'):
         # XXX Need to pass local parameters
-        gw = Gateway.get('sqlalchemy')(config={url: URL}, user=user, format=format)
+        gw = Gateway.get('sqlalchemy')(config={'url': URL}, user=user, format=format)
         gw.set_query(query)
         cb = Callback()
         gw.set_callback(cb)
