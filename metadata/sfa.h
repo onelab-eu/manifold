d8d7b135fe7da0c7d1348d990103a56d3ab86f81
Mohamed Larabi <mohamed.larabi@inria.fr>
2013-09-20 10:06:08 +0200

    - Modify resource object metadata. - Parse RSpecs using SFAWrap parsing code. - SFA compliant platforms should/would have supported RSpec type and version in their config.

--- metadata/sfa.h
+++ metadata/sfa.h
@@ -36,42 +36,56 @@ enum pl_distro {
     "n/a"
 };
 
-class resource {
-	const text  urn;
-##    text            arch;                   /**< Platform architecture */
-#    int             authority_id;           /**< The authority of the global PlanetLab federation that the site of the node belongs to */
-#    boot_state      boot_state;             /**< The current status of the node */
-#    unsigned        bw_limit;               /**< Bandwidth limits in effect on the node */
-#    unsigned        bw_unallocated;
-#    const int       client_id;
-    const int       component_id;
-    const int       component_manager_id;
-    const text      component_name;
-#    const text      disk_images;
-    bool            exclusive;
-##    const text      fcdistro;
-#    const text      hardware_types;
-    const text      hostname;               /**< Hostname */
-    const text      resource_hrn;                    /**< Human Readable name */
-#    const text      interfaces;
-    const int       latitude;               /**< Latitude */
-    const int       longitude;              /**< Longitude */
-    const network   network;
-#    const int       node_id;
-#    const bool      pl_initscripts;
-##    const pl_distro pldistro;               /**< Fedora or CentOS distribution to use for node or slivers */
-#    const text      services;
-    const int       site_id;
-#    const text      resource_type;
-    slice    slice[];
-    KEY(resource_hrn);
-	CAPABILITY(retrieve,join,fullquery);
+
+class location {
+    const text country;
+    const text longitude;
+    const text latitude;
+        CAPABILITY(retrieve,join);
 };
 
-#>>> prov - need
-#set(['load', 'interface.ipv4', 'cpuy', 'cpuw', 'services.login.authentication', 'cpum', 'services.login.username', 'city', 'reliabilityw', 'services.login.port', 'slicesm', 'slices', 'astype', 'bwm', 'memm', 'slicesw', 'type', 'reliabilityy', 'sliver', 'responsew', 'bww', 'mem', 'bwy', 'responsey', 'urn', 'memw', 'interface.client_id', 'fcdistro', 'responsem', 'memy', 'response', 'loadm', 'interface.component_id', 'country', 'region', 'services.login.hostname', 'slicesy', 'loady', 'asnumber', 'bw', 'reliabilitym', 'reliability', 'cpu', 'loadw'])
-#>>> need-prov
-#set(['disk_images', 'fc_distro', 'bw_limit', 'boot_state', 'interfaces', 'authority_id', 'bw_unallocated', 'hardware_types', 'node_id', 'pl_initscripts', 'client_id', 'services', 'resource_type'])
+class hardware_type {
+    const text name;
+        CAPABILITY(retrieve,join);
+};
+
+class interface {
+    const text component_id;
+        CAPABILITY(retrieve,join);
+};
+
+class initscript {
+    const text name;
+        CAPABILITY(retrieve,join);
+};
+
+class tag {
+    const text tagname;
+    const text value;
+        CAPABILITY(retrieve,join);
+};
+
+class resource {
+    const text  	urn;
+    const text  	hrn;
+    const text  	hostname;
+    const text		component_manager_id;
+    const text		component_id;
+    bool		exclusive;
+    const text		component_name;
+    hardware_type 	hardware_types[];
+    location 		location;
+    interface		interfaces[];
+    const text		boot_state;
+    initscript		initscripts[]; 		
+#    sliver		slivers[];
+#    service		services[];
+    location		location;
+    tag			tags[];  
+    slice		slices[];
+    KEY(hrn);
+        CAPABILITY(retrieve,join,fullquery);
+};
 
 
 class network {
