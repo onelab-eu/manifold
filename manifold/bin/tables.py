50d406d19af9240bb028aa2fe84350063158a269
Jordan Augé <jordan.auge@lip6.fr>
2013-11-05 17:09:24 +0100

    bin: added the actual code for the table command

--- manifold/bin/tables.py
+++ manifold/bin/tables.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python
+#! -*- coding: utf-8 -*-
 
-import json
+import sys, json
+from argparse             import ArgumentParser
+from manifold.bin.shell   import Shell
+from manifold.policy.rule import Rule
 
-from manifold.core.router import Router
-from manifold.core.query import Query
+METHOD_ADD = 1
 
-from manifold.policy.rule        import Rule
-from manifold.policy.target.drop import DropTarget
+DEFAULT_TABLE  = '*'
+DEFAULT_FIELDS = '*'
+DEFAULT_ACTION = 'RW'
 
-# A rule to prevent read access to a user's password
+def init_options():
+    usage="""%prog [options] [METHOD] [PARAMETERS]
+Issue an SFA call, using credentials from the manifold database."""
 
-rule = Rule()
+    parser = ArgumentParser()
 
-rule.object = 'local:user'
-rule.fields = set(['password'])
-rule.access = 'R'
-rule.target = 'DROP'
+    parser.add_argument("-A", "--add", action='store_const', 
+            const=METHOD_ADD, dest='method', required = True,
+            help = "TODO")
+    parser.add_argument("-o", "--object", dest='object',
+            default = DEFAULT_TABLE,
+            help = "TODO. Default is %s" % DEFAULT_TABLE)
+    parser.add_argument("-f", "--fields", dest='fields',
+            default = DEFAULT_FIELDS,
+            help = "TODO. Default is %s" % DEFAULT_FIELDS)
+    parser.add_argument("-a", "--access", dest='access',
+            default = DEFAULT_ACTION,
+            help = "TODO. Default is %s" % DEFAULT_ACTION)
+    parser.add_argument("-j", "--jump", dest='target', required = True,
+            help = "TODO")
 
-rule_dict = rule.to_dict()
-rule_params = {
-    'policy_json': json.dumps(rule_dict)
-}
+    return parser.parse_args()
 
-query = Query(action='create', object='local:policy', params=rule_params)
+def main():
+    args = init_options()
 
-# Instantiate a TopHat router
-with Router() as router:
-    router.forward(query)
+    if args.method == METHOD_ADD:
+        # Build rule dictionary
+        rule = Rule()
+        rule.object = args.object
+        rule.fields = args.fields
+        rule.access = args.access
+        rule.target = args.target
+        rule_dict = rule.to_dict()
+        
+        # Add rule
+        shell = Shell()
+        command = 'CREATE local:policy SET policy_json = "%s"' % json.dumps(rule_dict) 
+        shell.evaluate(command % locals())
+        shell.terminate()
+        
+    else:
+        raise Exception, 'Unknown method: %s' % args.method
 
-
-
+if __name__ == '__main__':
+    main()
