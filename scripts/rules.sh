c539894cafaf299c368c0e64fe6506521c1e7160
Jordan Augé <jordan.auge@lip6.fr>
2013-11-06 14:39:24 +0100

    scripts.rules: added flushing

--- scripts/rules.sh
+++ scripts/rules.sh
@@ -1,3 +1,4 @@
+manifold-tables -F
 manifold-tables -A -o local:user -f password -a R -j LOG
 manifold-tables -A -o local:user -f password -a R -j DROP
 manifold-tables -A -o \* 		 -f \* 		 -a R -j CACHE
