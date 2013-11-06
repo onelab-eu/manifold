ea597033c3f024d8bf5349d25ddc2f98181ede1c
Jordan Augé <jordan.auge@lip6.fr>
2013-11-06 14:07:09 +0100

    bin.tables: updated manifold-tables command to support iptables-like arguments

--- manifold/bin/tables.py
+++ manifold/bin/tables.py
@@ -2,59 +2,83 @@
 #! -*- coding: utf-8 -*-
 
 import sys, json
-from argparse             import ArgumentParser
-from manifold.bin.shell   import Shell
-from manifold.policy.rule import Rule
+from argparse              import ArgumentParser
+from manifold.bin.shell    import Shell
+from manifold.util.options import Options
+from manifold.util.log     import Log
+from manifold.policy.rule  import Rule
 
-METHOD_ADD = 1
+METHOD_APPEND = 1
+METHOD_FLUSH  = 2
+METHOD_INSERT = 3
+METHOD_DELETE = 4
 
 DEFAULT_TABLE  = '*'
 DEFAULT_FIELDS = '*'
 DEFAULT_ACTION = 'RW'
 
 def init_options():
-    usage="""%prog [options] [METHOD] [PARAMETERS]
-Issue an SFA call, using credentials from the manifold database."""
+    opt = Options()
 
-    parser = ArgumentParser()
-
-    parser.add_argument("-A", "--add", action='store_const', 
-            const=METHOD_ADD, dest='method', required = True,
+    # Method
+    group = opt.add_mutually_exclusive_group(required=True)
+    group.add_argument("-A", "--append", action='store_const', 
+            const=METHOD_APPEND, dest='method',
             help = "TODO")
-    parser.add_argument("-o", "--object", dest='object',
+    group.add_argument("-F", "--flush", action='store_const', 
+            const=METHOD_FLUSH, dest='method',
+            help = "TODO")
+    group.add_argument("-I", "--insert", action='store_const', 
+            const=METHOD_INSERT, dest='method',
+            help = "TODO")
+    group.add_argument("-D", "--delete", action='store_const', 
+            const=METHOD_DELETE, dest='method',
+            help = "todo")
+
+    # XXX positional argument for -I and -D ?
+
+    # Destination
+    opt.add_argument("-o", "--object", dest='object',
             default = DEFAULT_TABLE,
             help = "TODO. Default is %s" % DEFAULT_TABLE)
-    parser.add_argument("-f", "--fields", dest='fields',
+    opt.add_argument("-f", "--fields", dest='fields',
             default = DEFAULT_FIELDS,
             help = "TODO. Default is %s" % DEFAULT_FIELDS)
-    parser.add_argument("-a", "--access", dest='access',
+    opt.add_argument("-a", "--access", dest='access',
             default = DEFAULT_ACTION,
             help = "TODO. Default is %s" % DEFAULT_ACTION)
-    parser.add_argument("-j", "--jump", dest='target', required = True,
+
+    # Target
+    opt.add_argument("-j", "--jump", dest='target', required = True,
             help = "TODO")
 
-    return parser.parse_args()
-
 def main():
-    args = init_options()
+    init_options()
+    Shell.init_options()
 
-    if args.method == METHOD_ADD:
+    args = Options()
+
+    if args.method == METHOD_APPEND:
         # Build rule dictionary
         rule = Rule()
         rule.object = args.object
-        rule.fields = args.fields
+        fields = [x.strip() for x in args.fields.split(',')]
+        rule.fields = set(fields)
         rule.access = args.access
         rule.target = args.target
         rule_dict = rule.to_dict()
         
         # Add rule
+        # XXX We might need a support for transactions
         shell = Shell()
-        command = 'CREATE local:policy SET policy_json = "%s"' % json.dumps(rule_dict) 
-        shell.evaluate(command % locals())
+        command = "insert into local:policy SET policy_json = '%s'" % json.dumps(rule_dict)
+        print "command", command
+        shell.evaluate(command)
         shell.terminate()
         
     else:
-        raise Exception, 'Unknown method: %s' % args.method
+        raise Exception, "Not implemented"
+
 
 if __name__ == '__main__':
     main()
