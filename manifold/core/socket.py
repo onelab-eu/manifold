7e8117b8eb06edcb716d522b60806e1dec9751e7
Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
2013-11-14 16:31:52 +0100

    merge (wip)

---
+++ manifold/core/socket.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) UPMC Paris Universitas
+# Authors:
+#   Jordan Augé       <jordan.auge@lip6.fr>
+#   Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
+
+class Socket:
+    pass
