9621ef76a5de77db2eea47e318c84e401b90a65e
Jordan Augé <jordan.auge@lip6.fr>
2013-12-19 08:41:22 +0100

    metadata: added manifold tdmi

---
+++ metadata/manifold-tdmi.h
@@ -0,0 +1,25 @@
+class hop {
+    const inet        ip;             /**< IP discovered */
+    const unsigned    ttl;            /**< TTL value (0: source, 1: 1st hop ...) */
+    const unsigned    hop_probecount; /**< Identifies the hop among different paths (LB) */
+    const unsigned    path;           /**< Probe ID */
+
+    const ip          source;
+    const ip          destination;
+    KEY(source, destination, ttl);
+
+    CAPABILITY(retrieve, join);
+};
+
+class traceroute {
+    ip       source;          /**< The measurement agent */
+    ip       destination;    /**< The target IP */
+    hop         hops[];         /**< IP hops discovered on the measurement */
+    unsigned    hop_count;      /**< Number of IP hops */
+    timestamp   first;          /**< Birth date of this IP path */
+    timestamp   last;           /**< Death date of this IP path */
+    unsigned    ts;
+
+    CAPABILITY(join);
+    KEY(source, destination);
+};
