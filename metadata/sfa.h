5d2520d5a3801de74c84c5735038762a790f78e2
Loic Baron <loic.baron@lip6.fr>
2014-02-25 11:23:47 +0100

    added slice metadata to advertise the available fields

--- metadata/sfa.h
+++ metadata/sfa.h
@@ -1,8 +1,15 @@
 class slice {
-    const text  slice_urn;   /**< Slice Human Readable name */
-    const text  slice_hrn;   /**< Slice Human Readable name */
-    const text  slice_type;
+    const text slice_urn;   /**< Slice Human Readable name */
+    const text slice_hrn;   /**< Slice Human Readable name */
+    const text slice_type;
+    const text slice_description;
+    const text created;
+    const text slice_expires;
+    const text slice_last_updated;
+    const text nodes;
+    const text slice_url;
     const authority parent_authority;
+
 # lease       lease[];       /**< List of leases associated to the slice */
     user        user[];        /**< List of users associated to the slice */
 
