1d7b79b076cd9a2a238de1df6c2a77438ee5a742
Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
2014-03-05 18:23:44 +0100

    Fix build

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 */
+# -*- coding:utf-8 -*-
+#
+# dispatcher build script.
+#
+# Usage:
+#     cd ~/git/tdmi/packages/dispatcher
+#     python setup.py install 
+#     python setup.py bdist_rpm
+#
+# Authors:
+#    Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
+#
+# Copyright UPMC Paris Universitas
 
 import os
-from glob       import glob
-from setuptools import find_packages, setup
 from platform   import dist
+from setuptools import find_packages, setup
 
 import manifold
 
-distribution, _, _ = dist()
-
-ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-# The following makes fail: "python setup.py bdist_rpm"
-# The MANIFEST file is generated dynamicaly by the Makefile which is wrong, we should only write a MANIFEST.in file
-# and build rpm packages using "python setup.py bdist_rpm"
-#long_description = open(os.path.join(ROOT_PATH, 'README.rst')).read()
-long_description = ""
+distribution, _, _ = dist()                                       
+ROOT_PATH          = os.path.abspath(os.path.dirname(__file__))
+long_description   = open(os.path.join(ROOT_PATH, "README.rst")).read()
 
 etc_sysconfig = (
     "/etc/sysconfig/manifold-xmlrpc",
@@ -27,70 +33,42 @@ etc_initd = (
     ["manifold/etc/etc_init.d_xmlrpc-%s" % distribution]
 )
 
-name             = "manifold"
-install_requires = ["cfgparse", "networkx"] if distribution == "Fedora" else ["cfgparse"]
-
-version          = '.'.join(["%s" % x for x in manifold.__version__])
-provides         = ["%(name)s (%(version)s)" % locals()]
-
-# http://devblog.4teamwork.ch/blog/2013/06/20/setuptools-console-scripts-entrypoints-and-buildout/
-entry_points = """
-[console_scripts]
-manifold-shell             = manifold.bin.shell:main
-manifold-xmlrpc            = manifold.bin.xmlrpc:main
-manifold-sfa-delegate      = manifold.bin.delegate:main
-manifold-init-db           = manifold.bin.initdb:main
-manifold-add-user          = manifold.bin.adduser:main
-manifold-add-account       = manifold.bin.addaccount:main
-manifold-add-platform      = manifold.bin.addplatform:main
-manifold-disable-platform  = manifold.bin.disableplatform:main
-manifold-enable-platform   = manifold.bin.enableplatform:main
-manifold-upload-credential = manifold.bin.uploadcredential:main
-"""
-
 setup(
-    name             = name,
-    version          = version,
-    description      = "Manifold Interconnection Framework",
-    long_description = long_description,
-    author           = "Jordan Augé, Marc-Olivier Buob",
-    url              = "http://www.top-hat.info",
-    provides         = provides,
+    name               = "manifold-xmlrpc",
+    version            = ".".join(["%s" % x for x in manifold.__version__]),
+    description        = "Manifold Backend",
+    long_description   = long_description,
+    author             = "TopHat team",
+    author_email       = "support@top-hat.info",
+    url                = "http://git.top-hat.info/?p=manifold.git",
+    license            = "GPLv3",
+    zip_safe           = False,
+    packages           = find_packages(),
     namespace_packages = ["manifold"],
-    #setup_requires  = ['nose>=0.11',],
-    #test_requires   = ['unittest2>=0.5.1',
-    #    'mockito==0.5.1',
-    #    'python-faker==0.2.3', # jordan: faker
-    #    'factory-boy==1.0.0',],
-    install_requires = install_requires,
-    license          = "GPLv3",
-    packages         = find_packages(),
-    data_files       = [
+    data_files = [
         # Do not forget to reference those files in MANIFEST.in
         # Do not use symlinks with setuptool (those files will be ignored)
         # Do not use hardlinks with git (not supported)
-        ('/usr/share/manifold/metadata/', glob('metadata/*.h')),
+        ("/etc/manifold", [
+            "manifold/etc/manifold_manifold.conf",
+        ]),
         etc_initd,
-        etc_sysconfig,
+        etc_sysconfig
     ],
-#    scripts = [
-#        'scripts/manifold-reset-db.sh',
-#        'scripts/manifold-populate-db.sh'
-#    ] #OBSOLETE| + glob(("clientbin/*"),
-#    ,
-#    entry_points = {
-#        'console_scripts': [
-#            'manifold-shell = manifold.bin.shell:main',
-#            'manifold-xmlrpc = manifold.bin.xmlrpc:main',
-#            'manifold-sfa-delegate = manifold.bin.delegate:main',
-#            'manifold-init-db = manifold.bin.initdb:main',
-#            'manifold-add-user = manifold.bin.adduser:main',
-#            'manifold-add-account = manifold.bin.addaccount:main',
-#            'manifold-add-platform = manifold.bin.addplatform:main',
-#            'manifold-disable-platform = manifold.bin.disableplatform:main',
-#            'manifold-enable-platform = manifold.bin.enableplatform:main',
-#            'manifold-upload-credential = manifold.bin.uploadcredential:main',
-#        ],
-#    },
-#    entry_points = entry_points,
+    install_requires   = ["setuptools", "cfgparse", "python-daemon"],
+    entry_points = {
+        "console_scripts": [
+            'manifold-shell = manifold.bin.shell:main',
+            'manifold-xmlrpc = manifold.bin.xmlrpc:main',
+            'manifold-sfa-delegate = manifold.bin.delegate:main',
+            'manifold-init-db = manifold.bin.initdb:main',
+            'manifold-add-user = manifold.bin.adduser:main',
+            'manifold-add-account = manifold.bin.addaccount:main',
+            'manifold-add-platform = manifold.bin.addplatform:main',
+            'manifold-disable-platform = manifold.bin.disableplatform:main',
+            'manifold-enable-platform = manifold.bin.enableplatform:main',
+            'manifold-upload-credential = manifold.bin.uploadcredential:main',
+
+        ],
+    },
 )
