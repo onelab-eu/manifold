3aced7185dcf9699cd94ca6a5f9015ed107861fc
Jordan Augé <jordan.auge@lip6.fr>
2014-03-11 17:33:34 +0100

    added tests

---
+++ tests/test_gateways_process_ping.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from manifold.core.annotation               import Annotation
+from manifold.core.packet                   import QueryPacket
+from manifold.core.query                    import Query
+from manifold.core.sync_receiver            import SyncReceiver
+
+from manifold.gateways.process.objects.ping import PingGateway
+
+
+def main():
+
+    ping_gw = PingGateway()
+
+    print ping_gw.make_announce()
+
+    query       = Query.get('ping').filter_by('destination', '==', '8.8.8.8')
+    annotation  = Annotation()
+    receiver    = SyncReceiver()
+    packet      = QueryPacket(query, annotation, receiver)
+    
+    ping_gw.receive(packet)
+    print receiver.get_result_value()
+
+if __name__ == '__main__':
+    main()
