20a97c4773cf25e2f6644ca6ea98d7d96df04ac8
Jordan Augé <jordan.auge@lip6.fr>
2014-03-13 11:56:54 +0100

    bin: added bases for client, router and agent gateways: made ping a real gateway

---
+++ manifold/bin/client.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import asyncore, asynchat
+import socket, time
+
+from manifold.core.query        import Query
+from manifold.core.packet       import Packet, QueryPacket, ErrorPacket # XXX
+from manifold.core.annotation   import Annotation
+
+class State(object): pass
+
+class Client(asynchat.async_chat):
+
+    STATE_LENGTH = State()
+    STATE_PACKET = State()
+
+    path = '/tmp/manifold'
+
+    def __init__(self):
+        asynchat.async_chat.__init__(self)
+
+        self.data = ""
+
+
+        self.create_socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._request_fifo = [] # UNUSED ?
+        self._receive_buffer = []
+
+        # Prepare packet reception (DUP)
+        self._pstate = self.STATE_LENGTH
+        self.set_terminator (8)
+
+        self.connect(self.path)
+
+
+
+    def log (self, *items):
+        print "UNUSED log?"
+        print "log ", self.__class__, items
+
+
+    def handle_connect (self):
+        # Push a query
+        query = Query.get('ping').filter_by('destination', '==', '8.8.8.8')
+        annotation = Annotation()
+        
+        packet = QueryPacket(query, annotation, None)
+
+        packet_str = packet.serialize()
+        self.push ('%08x%s' % (len(packet_str), packet_str))
+
+
+    def close(self):
+        asynchat.async_chat.close(self)
+
+
+    def collect_incoming_data (self, data):
+        self._receive_buffer.append (data)
+
+    def found_terminator (self):
+        self._receive_buffer, data = [], ''.join (self._receive_buffer)
+
+        if self._pstate is self.STATE_LENGTH:
+            packet_length = int(data, 16)
+            self.set_terminator(packet_length)
+            self._pstate = self.STATE_PACKET
+        else:
+            # We shoud wait until last record
+            packet = Packet.deserialize(data)
+
+            print "PACKET=", packet
+            if isinstance(packet, ErrorPacket):
+                print packet._traceback
+
+            if packet.is_last():
+                self.close()
+            else:
+                # Prepare for next packet
+                self.set_terminator (8)
+                self._pstate = self.STATE_LENGTH
+
+c = Client()
+asyncore.loop()
