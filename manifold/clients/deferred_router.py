60dcac9f1dcb535261f9f600628fe839b26153ba
Jordan Augé <jordan.auge@lip6.fr>
2014-04-12 15:31:39 +0200

    fixed import in DeferredRouter

--- manifold/clients/deferred_router.py
+++ manifold/clients/deferred_router.py
@@ -1,6 +1,7 @@
 from manifold.clients.router import ManifoldRouterClient
-
+from manifold.core.annotation       import Annotation
 from manifold.core.deferred_receiver import DeferredReceiver
+from manifold.core.packet           import Packet, QueryPacket
 
 class ManifoldDeferredRouterClient(ManifoldRouterClient):
     
