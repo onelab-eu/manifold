1eccb2c83d514830bcc6e02f628a02d79143f66c
Jordan Augé <jordan.auge@lip6.fr>
2014-04-15 01:35:10 +0200

    updates

--- manifold/core/xmlrpc_api.py
+++ manifold/core/xmlrpc_api.py
@@ -20,7 +20,7 @@ from twisted.web                        import xmlrpc
 from manifold.auth                      import Auth
 from manifold.core.annotation           import Annotation
 from manifold.core.code                 import CORE, ERROR, FORBIDDEN
-from manifold.core.deferred_receiver    import DeferredReceiver
+#from manifold.core.deferred_receiver    import DeferredReceiver
 from manifold.core.packet               import QueryPacket
 from manifold.core.query                import Query
 from manifold.core.result_value         import ResultValue
