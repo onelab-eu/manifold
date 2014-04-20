9824ca79612e82c7771b47990f8682d8e9fb4f05
Jordan Augé <jordan.auge@lip6.fr>
2014-04-20 17:45:10 +0200

    fixed traceroute location

--- manifold/gateways/process/paristraceroute.py
+++ manifold/gateways/process/paristraceroute.py
@@ -329,4 +329,3 @@ class ParisTracerouteGateway(ProcessGateway):
 
     #path = '/usr/local/bin/paris-traceroute'
     path = '/usr/sbin/paris-traceroute' 
-    path = '/bin/paris-traceroute' # FAKE
