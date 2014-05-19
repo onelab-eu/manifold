ae1fa4c060ea508e55c6927a7f557b5b835ab045
Loic Baron <loic.baron@lip6.fr>
2014-05-19 11:55:33 +0200

    Tests: updated the functional test for reservable resources, prefixing the testbed concerned by the test (either Nitos or Iot-Lab at the moment)

--- tests/functional_tests/reservable.py
+++ tests/functional_tests/reservable.py
@@ -22,7 +22,7 @@ from manifold.util.test     import ManifoldTestCase
 from manifold.util.options  import Options
 
 test_options = {
-   'log_level': 'CRITICAL', # DEBUG, INFO, WARNING, ERROR, CRITICAL
+   'log_level': 'DEBUG', # DEBUG, INFO, WARNING, ERROR, CRITICAL
    'verbosity': 2, # 0
 }
 
@@ -47,7 +47,7 @@ class TestbedWithReservationTestCase(ManifoldTestCase):
         """
         List all the resources from the testbed
         """
-        Q = 'SELECT hrn, type FROM resource'
+        Q = 'SELECT hrn, type FROM %sresource' % (self.NAMESPACE, )
         records = self._run(Q)
 
         # Let's check we have the requested fields (this should not be part of this test ?)
@@ -61,18 +61,18 @@ class TestbedWithReservationTestCase(ManifoldTestCase):
         """
         List all the leases from the testbed
         """
-        Q = 'SELECT resource, start_time, end_time FROM lease'
+        Q = 'SELECT resource, start_time, end_time FROM %slease' % (self.NAMESPACE, )
         records = self._run(Q)
 
         # We might count the leases, add one, and check that we have one more...
 
     def test_list_slice_resources(self):
-        Q = 'SELECT hrn FROM resource WHERE slice == "%s"' % (self.SLICE_URN, )
+        Q = 'SELECT hrn FROM %sresource WHERE slice == "%s"' % (self.NAMESPACE, self.SLICE_URN, )
         records = self._run(Q)
         
 
     def test_list_slice_leases(self):
-        Q = 'SELECT resource, start_time, end_time FROM lease WHERE slice == "%s"' % (self.SLICE_URN, )
+        Q = 'SELECT resource, start_time, end_time FROM %slease WHERE slice == "%s"' % (self.NAMESPACE, self.SLICE_URN, )
         records = self._run(Q)
 
     def test_add_lease(self):
@@ -92,32 +92,32 @@ class TestbedWithReservationTestCase(ManifoldTestCase):
           Cannot lease 'omf.nitos.node037', because it is unavailable for the
           requested time. in method CreateSliver">
         """
-        Q = 'UPDATE slice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NODE_1, self.NODE_1, self.START, self.END, self.SLICE_HRN, )
+        Q = 'UPDATE %sslice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NAMESPACE, self.NODE_1, self.NODE_1, self.START, self.END, self.SLICE_HRN, )
         records = self._run(Q)
 
     def test_add_two_leases(self):
         """
         Same resource twice, not in resources (no need here).
         """
-        Q = 'UPDATE slice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NODE_1, self.NODE_1, self.START, self.END, self.NODE_1, self.START+3600, self.END+3600, self.SLICE_HRN, )
+        Q = 'UPDATE %sslice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NAMESPACE, self.NODE_1, self.NODE_1, self.START, self.END, self.NODE_1, self.START+3600, self.END+3600, self.SLICE_HRN, )
         records = self._run(Q)
 
     def test_add_two_leases_2(self):
         """
         Different resources, one is not in resources.
         """
-        Q = 'UPDATE slice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NODE_1, self.NODE_1, self.START, self.END, self.NODE_2, self.START+3600, self.END+3600, self.SLICE_HRN, )
+        Q = 'UPDATE %sslice SET resource = ["%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NAMESPACE, self.NODE_1, self.NODE_1, self.START, self.END, self.NODE_2, self.START+3600, self.END+3600, self.SLICE_HRN, )
         records = self._run(Q)
 
     def test_add_two_leases_3(self):
         """
         Different resources, both in resources.
         """
-        Q = 'UPDATE slice SET resource = ["%s", "%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NODE_1, self.NODE_2, self.NODE_1, self.START, self.END, self.NODE_2, self.START+3600, self.END+3600, self.SLICE_HRN, )
+        Q = 'UPDATE %sslice SET resource = ["%s", "%s"], lease = [{resource: "%s", start_time: "%s", end_time: "%s"}, {resource: "%s", start_time: "%s", end_time: "%s"}] where slice_hrn == "%s"' % (self.NAMESPACE, self.NODE_1, self.NODE_2, self.NODE_1, self.START, self.END, self.NODE_2, self.START+3600, self.END+3600, self.SLICE_HRN, )
         records = self._run(Q)
 
     def test_clear_slice(self):
-        Q = 'UPDATE slice SET resource = [], lease = [] where slice_hrn == "%s"' % (self.SLICE_HRN, )
+        Q = 'UPDATE %sslice SET resource = [], lease = [] where slice_hrn == "%s"' % (self.NAMESPACE, self.SLICE_HRN, )
         records = self._run(Q)
 
         assert len(records) == 1
@@ -131,13 +131,15 @@ class TestbedWithReservationTestCase(ManifoldTestCase):
         assert len(first_record['lease']) == 0
 
 class NITOSTests(TestbedWithReservationTestCase):
-    NAMESPACE = 'nitosb'
+    NAMESPACE = 'nitos:'
     SLICE_HRN = 'ple.upmc.myslicedemo'
     SLICE_URN = 'urn:publicid:IDN+ple:upmc+slice+myslicedemo'
 
     GRAIN = 1800
-    NODE_1 = 'urn:publicid:IDN+omf:nitos+node+omf.nitos.node037'
-    NODE_2 = 'urn:publicid:IDN+omf:nitos+node+omf.nitos.node036'
+    #NODE_1 = 'urn:publicid:IDN+omf:nitos+node+omf.nitos.node037'
+    NODE_1 = 'urn:publicid:IDN+omf:nitos+node+node047'
+    #NODE_2 = 'urn:publicid:IDN+omf:nitos+node+omf.nitos.node036'
+    NODE_2 = 'urn:publicid:IDN+omf:nitos+node+node041'
 
     t = time.time()
     START = int(t - (t % GRAIN) + GRAIN) 
@@ -160,7 +162,7 @@ class NITOSTests(TestbedWithReservationTestCase):
         assert has_nodes and has_channels
 
 class IoTLABTests(TestbedWithReservationTestCase):
-    NAMESPACE = 'iotlab'
+    NAMESPACE = 'iotlab:'
     SLICE_HRN = 'ple.upmc.myslicedemo'
     SLICE_URN = 'urn:publicid:IDN+ple:upmc+slice+myslicedemo'
 
