fc3d8e074bd1bc18a5e1befc207be8c98a52f883
Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
2014-10-10 17:24:40 +0200

    bumped version

--- manifold/__init__.py
+++ manifold/__init__.py
@@ -4,4 +4,4 @@ Licensed under GLPv3
 """
 # Please keep __version__ in sync with %{version} in manifold.spec
 # (This version is currently automatically handled by the RPM package script)
-__version__ = (2, 0, 74)
+__version__ = (2, 0, 76)
