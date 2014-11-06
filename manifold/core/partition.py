0e0ad82cc58346f322b34ba0821ff4fb04dfaf4a
Jordan Augé <jordan.auge@lip6.fr>
2014-11-06 15:05:21 +0100

    fixing serialization of partitions

--- manifold/core/partition.py
+++ manifold/core/partition.py
@@ -8,10 +8,17 @@ class Partitions(set):
     Partitions are a set of filter.
     """
 
+    @staticmethod
+    def from_list(self):
+        ret = Partitions()
+        for partition_list in self:
+            ret.add(Partition.from_list(partition_list))
+        return ret
+             
     def to_list(self):
         ret = list()
         for partition in self:
-            ret.append(partition.to_dict())
+            ret.append(partition.to_list())
         return ret
     
 
