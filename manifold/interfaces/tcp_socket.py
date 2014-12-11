ab41cedaf0f554a2bb16c9f134190bf1ea29ce4d
Jordan Augé <jordan.auge@lip6.fr>
2014-12-11 14:48:50 +0100

    wip

--- manifold/interfaces/tcp_socket.py
+++ manifold/interfaces/tcp_socket.py
@@ -45,7 +45,6 @@ class ManifoldProtocol(IntNStringReceiver):
 
     def stringReceived(self, msg):
         packet = Packet.deserialize(msg)
-        print "PACKET RECV BY PROTO", packet
         self.receive(packet)
 
     def send_packet(self, packet):
@@ -194,7 +193,6 @@ class TCPServerSocketFactory(ServerFactory):
         self._router = router
 
     def buildProtocol(self, addr):
-        print "RECEIVED CONNECTION FROM CLIENT", addr
         p = self.protocol(self._router)
         p.factory = self
         return p
